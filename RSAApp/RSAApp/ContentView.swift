6125f55b7ab312391e8dc836c484dd387c63ed92
Lillian Yang <lyang@lcs.on.ca>
2022-04-21 10:55:17 -0400

    Started

---
+++ RSAApp/RSAApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RSAApp
+//
+//  Created by Lillian Yang on 2022-04-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RSAApp/RSAApp/RSAAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSAAppApp.swift
+//  RSAApp
+//
+//  Created by Lillian Yang on 2022-04-21.
+//
+
+import SwiftUI
+
+@main
+struct RSAAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
